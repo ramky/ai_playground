4d445b825fa477b369835f1baa69edbbaceb649e
Ram Iyer <ramky.iyer@gmail.com>
2023-11-22 14:13:57 -0500

    adding output_parsers and linkedin_data stub

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,8 +5,11 @@ from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
+from output_parsers import person_intel_parser, PersonIntel
+from tools.others.linkedin_data_stub import linkedin_data_stub
 
 DEBUG = False
+SIMULATE = True
 
 
 def ice_break(name: str) -> str:
@@ -18,22 +21,37 @@ def ice_break(name: str) -> str:
         given the information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        3. A topic that may interest them
+        Format instructions:
+        {format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
-    if DEBUG:
-        print(f"linkedin_data is {linkedin_data}")
+    if SIMULATE:
+        linkedin_data = scrape_linkedin_profile(
+            linkedin_profile_url=linkedin_profile_url
+        )
+        if DEBUG:
+            print(f"linkedin_data is {linkedin_data}")
+    else:
+        linkedin_data = linkedin_data_stub
 
-    print(chain.run(information=linkedin_data))
+    result = chain.run(information=linkedin_data)
+
+    return person_intel_parser.parse(result)
 
 
 if __name__ == "__main__":
-    # ice_break(name="Vidyut Latay")
-    ice_break(name="Harrison Chase")
+    # result = ice_break(name="Vidyut Latay")
+    result = ice_break(name="Harrison Chase")
+    print(f"result is {result}")
